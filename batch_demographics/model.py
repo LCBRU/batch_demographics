7731534d895b2574c568ec3879dc8408ab7aebef
Richard Bramley <rabramley@gmail.com>
2018-01-12 13:45:10 +0000

    Remove unnecessary constructor

--- batch_demographics/model.py
+++ batch_demographics/model.py
@@ -5,6 +5,3 @@ class Batch(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100), unique=True)
-
-    def __init__(self, *args, **kwargs):
-        self.name = kwargs.get('name')
